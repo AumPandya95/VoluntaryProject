e50ed3a2e55c26435f9c2fed24574252c38cd706
Aum Pandya <aumpandya.95@gmail.com>
2018-10-16 22:57:24 +0530

    Error adding to DataFrame rectified

--- Stat_Code.py
+++ Stat_Code.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 increase_rate = 0.02
 date_limit = 7
+count = 0
 
 data_source =data = pd.read_csv("D:\Python\VoluntaryWork\Data.csv",header=0, sep=",", index_col=0, parse_dates = True)
 data= data.reset_index()
@@ -21,12 +22,12 @@ data_sam['s_cre'] =data_sam['creatinine'].shift().fillna(0) #shift
 data_sam['c_cre'] = ((data_sam['creatinine'] - data_sam['s_cre'])/data_sam['s_cre'])*100
 data_sam['c_alb'] = ((data_sam['albumin'] - data_sam['s_alb'])/data_sam['s_alb'])*100
 data_sam= data_sam.reset_index()
-result=pd.DataFrame()
-pd.DataFrame(result, columns=('Date','Type' ,'IncreaseCount', 'IncreasePresentageAvg','DecreaseCount', 'DecreasePresentageAvg'))
+result=pd.DataFrame(columns = ['date','type','inc_count','inc_pre_avg','dec_count','dec_per_avg'])
+#pd.DataFrame(result, columns=('Date','Type' ,'IncreaseCount', 'IncreasePresentageAvg','DecreaseCount', 'DecreasePresentageAvg'))
 
 for itr in range(0,len(data_sam.index)-date_limit-1):
     rbcsummary = {
-           "date":data.iat[itr,1],
+           "date":data_sam.iat[itr,1],
            "type": "RBC",
            "inc_count":0,
            "inc_pre_avg":0,
@@ -37,6 +38,7 @@ for itr in range(0,len(data_sam.index)-date_limit-1):
 #using .iat[] instead of .get_value() as the latter is deprecated and will be removed in a future release
     percentinc= percentdec=0
     if(data_sam.iat[itr,10] >= increase_rate and data_sam.iat[itr,9] >=increase_rate):
+        count += 1
         #Find a way to remove the row[0] as value of c_alb and c_creatinine is inf
         for itr1 in range(1,date_limit):
             #print(itr," ",itr1," ",data.get_value(itr+itr1,'rbc')," ",data.get_value(itr+itr1-1,'rbc'))
@@ -48,4 +50,6 @@ for itr in range(0,len(data_sam.index)-date_limit-1):
                 rbcsummary["dec_count"]+=1
                 percentdec+=((data_sam.iat[itr+itr1,5]-data_sam.iat[itr+itr1-1,5])/data_sam.iat[itr+itr1,5])*100
                 rbcsummary["dec_per_avg"]=percentdec/rbcsummary["dec_count"]
-        result.append(pd.DataFrame.from_dict(rbcsummary),ignore_index=True)
+        res = pd.DataFrame([rbcsummary],columns = ['date','type','inc_count','inc_pre_avg','dec_count','dec_per_avg'],index = [count])
+        result = pd.concat([result,res],axis = 0)
+    
