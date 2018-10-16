849fd2d8aac16dd568d3b9fec1a641f7d4b05391
Aum Pandya <aumpandya.95@gmail.com>
2018-10-17 05:06:38 +0530

    Commented out the extra lines for now. Will remove it once the testing is done.

--- Stat_Code.py
+++ Stat_Code.py
@@ -45,6 +45,7 @@ def calc_engine(str):
                }
         percentinc= percentdec=0
         if(data.loc[itr,'c_alb'] >= increase_rate and data.loc[itr,'c_cre'] >=increase_rate):
+            count += 1
             for itr1 in range(1,date_limit):
                 #print(itr," ",itr1," ",data.get_value(itr+itr1,'rbc')," ",data.get_value(itr+itr1-1,'rbc'))
                 if(data.loc[itr+itr1,str]>data.loc[itr+itr1-1,str]):
@@ -62,21 +63,23 @@ def calc_engine(str):
     result.to_excel(writer, sheet_name = str)
     writer.save()
     return 0
-    #using .iat[] instead of .get_value() as the latter is deprecated and will be removed in a future release
-        percentinc= percentdec=0
-        if(data.iat[itr,10] >= increase_rate and data.iat[itr,9] >=increase_rate):
-            count += 1
-            #Find a way to remove the row[0] as value of c_alb and c_creatinine is inf
-            for itr1 in range(1,date_limit):
-                #print(itr," ",itr1," ",data.get_value(itr+itr1,'rbc')," ",data.get_value(itr+itr1-1,'rbc'))
-                if(data.iat[itr+itr1,5]>data.iat[itr+itr1-1,5]):
-                    rbcsummary["inc_count"]+=1
-                    percentinc+=((data.iat[itr+itr1,5]-data.iat[itr+itr1-1,5])/data.iat[itr+itr1,5])*100
-                    rbcsummary["inc_pre_avg"]=percentinc/rbcsummary["inc_count"]
-                elif(data.iat[itr+itr1,5]<data.iat[itr+itr1-1,5]):
-                    rbcsummary["dec_count"]+=1
-                    percentdec+=((data.iat[itr+itr1,5]-data.iat[itr+itr1-1,5])/data.iat[itr+itr1,5])*100
-                    rbcsummary["dec_per_avg"]=percentdec/rbcsummary["dec_count"]
-            res = pd.DataFrame([rbcsummary],columns = ['date','type','inc_count','inc_pre_avg','dec_count','dec_per_avg'],index = [count])
-            result = pd.concat([result,res],axis = 0)
+    #using .loc[] instead of .get_value() as the latter is deprecated and will be removed in a future release
+# =============================================================================
+#         percentinc= percentdec=0
+#         if(data.iat[itr,10] >= increase_rate and data.iat[itr,9] >=increase_rate):
+#             count += 1
+#             #Find a way to remove the row[0] as value of c_alb and c_creatinine is inf
+#             for itr1 in range(1,date_limit):
+#                 #print(itr," ",itr1," ",data.get_value(itr+itr1,'rbc')," ",data.get_value(itr+itr1-1,'rbc'))
+#                 if(data.iat[itr+itr1,5]>data.iat[itr+itr1-1,5]):
+#                     rbcsummary["inc_count"]+=1
+#                     percentinc+=((data.iat[itr+itr1,5]-data.iat[itr+itr1-1,5])/data.iat[itr+itr1,5])*100
+#                     rbcsummary["inc_pre_avg"]=percentinc/rbcsummary["inc_count"]
+#                 elif(data.iat[itr+itr1,5]<data.iat[itr+itr1-1,5]):
+#                     rbcsummary["dec_count"]+=1
+#                     percentdec+=((data.iat[itr+itr1,5]-data.iat[itr+itr1-1,5])/data.iat[itr+itr1,5])*100
+#                     rbcsummary["dec_per_avg"]=percentdec/rbcsummary["dec_count"]
+#             res = pd.DataFrame([rbcsummary],columns = ['date','type','inc_count','inc_pre_avg','dec_count','dec_per_avg'],index = [count])
+#             result = pd.concat([result,res],axis = 0)
+# =============================================================================
         
